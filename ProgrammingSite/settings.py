6bb6e8a7d38ae8b24620173c761d5c92c334c20d
Ali Samiee <ali.samiee.developer@gmail.com>
2025-04-12 13:56:54 +0330

    Account and company apps were added for users and companies.

--- ProgrammingSite/settings.py
+++ ProgrammingSite/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #External Apps:
+    'Account',
+    'Company',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
