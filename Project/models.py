0096a949f332c502fce9123262f7eafa90fa1dd6
Ali Samiee <ali.samiee.developer@gmail.com>
2025-04-12 19:22:14 +0330

    The project class was created and the desired fields were defined.

--- Project/models.py
+++ Project/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    owner = models.ForeignKey('Account.User', on_delete=models.CASCADE, related_name='projects')
+    title = models.CharField(max_length=55)
+    description = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
