9ae4f21aaa384f172a987de87143c28cd52151b6
Ali Samiee <ali.samiee.developer@gmail.com>
2025-04-14 22:16:03 +0330

    A serializer was created to log in the user and its validate method was also rewritten.

--- Account/serializers.py
+++ Account/serializers.py
@@ -20,3 +20,16 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])  # هش کردن گذرواژه
         user.save()
         return user
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        email = data.get('email')
+        password = data.get('password')
+        user = authenticate(email=email, password=password)
+        if user is None:
+            raise serializers.ValidationError("email or password is wrong!")
+        data['user'] = user
+        return data
